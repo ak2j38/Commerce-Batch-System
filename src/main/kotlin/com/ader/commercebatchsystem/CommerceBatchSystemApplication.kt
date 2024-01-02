e8a0ebd76023643422b5b09ca1ce467e73d53adb
WooJin Park(Ader) <ak2j38@gmail.com>
2024-01-02 22:21:50 +0900

    First commit

---
+++ src/main/kotlin/com/ader/commercebatchsystem/CommerceBatchSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.ader.commercebatchsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommerceBatchSystemApplication
+
+fun main(args: Array<String>) {
+  runApplication<CommerceBatchSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/ader/commercebatchsystem/CommerceBatchSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ader.commercebatchsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommerceBatchSystemApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
