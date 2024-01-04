703f7a5a0173a89cd4abd72591720d4744f0105b
WooJin Park(Ader) <ak2j38@gmail.com>
2024-01-04 12:43:57 +0900

    feat: 주문 상품, 주문상품 스냅샷 테이블 추가

---
+++ src/main/kotlin/com/ader/commercebatchsystem/domain/entity/OrderItem.kt
@@ -0,0 +1,61 @@
+package com.ader.commercebatchsystem.domain.entity
+
+import jakarta.persistence.*
+import java.time.ZonedDateTime
+
+@Entity
+class OrderItem(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_item_no")
+    val id: Long? = null,
+    val orderNo: Long,
+    @Column(name = "order_item_snapshot_no")
+    val orderItemSnapshotNo: Long,
+    val orderCount: Int? = 1,
+    val itemDeliveryStatus: Int? = 0,
+    val createdAt: ZonedDateTime? = ZonedDateTime.now(),
+    val updatedAt: ZonedDateTime? = ZonedDateTime.now(),
+    val deletedAt: ZonedDateTime? = null,
+    val purchaseConfirmedAt: ZonedDateTime? = null,
+    val shippedCompleteAt: ZonedDateTime? = null,
+    @OneToOne
+    @JoinColumn(name = "order_item_snapshot_no", insertable = false, updatable = false)
+    val orderItemSnapshot: OrderItemSnapshot,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as OrderItem
+
+        if (id != other.id) return false
+        if (orderNo != other.orderNo) return false
+        if (orderItemSnapshotNo != other.orderItemSnapshotNo) return false
+        if (orderCount != other.orderCount) return false
+        if (itemDeliveryStatus != other.itemDeliveryStatus) return false
+        if (createdAt != other.createdAt) return false
+        if (updatedAt != other.updatedAt) return false
+        if (deletedAt != other.deletedAt) return false
+        if (purchaseConfirmedAt != other.purchaseConfirmedAt) return false
+        if (shippedCompleteAt != other.shippedCompleteAt) return false
+        if (orderItemSnapshot != other.orderItemSnapshot) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + orderNo.hashCode()
+        result = 31 * result + orderItemSnapshotNo.hashCode()
+        result = 31 * result + (orderCount ?: 0)
+        result = 31 * result + (itemDeliveryStatus ?: 0)
+        result = 31 * result + (createdAt?.hashCode() ?: 0)
+        result = 31 * result + (updatedAt?.hashCode() ?: 0)
+        result = 31 * result + (deletedAt?.hashCode() ?: 0)
+        result = 31 * result + (purchaseConfirmedAt?.hashCode() ?: 0)
+        result = 31 * result + (shippedCompleteAt?.hashCode() ?: 0)
+        result = 31 * result + orderItemSnapshot.hashCode()
+        return result
+    }
+}
---
+++ src/main/kotlin/com/ader/commercebatchsystem/domain/entity/OrderItemSnapshot.kt
@@ -0,0 +1,83 @@
+package com.ader.commercebatchsystem.domain.entity
+
+import com.ader.commercebatchsystem.domain.enums.TaxType
+import com.ader.commercebatchsystem.domain.enums.TaxTypeConverter
+import jakarta.persistence.*
+import java.math.BigDecimal
+import java.time.ZonedDateTime
+
+@Entity
+class OrderItemSnapshot(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_item_snapshot_no")
+    val id: Long? = null,
+    val createdAt: ZonedDateTime? = ZonedDateTime.now(),
+    val updatedAt: ZonedDateTime? = ZonedDateTime.now(),
+    val deletedAt: ZonedDateTime? = null,
+    @Column(name = "product_no")
+    val productNo: Long,
+    @Column(name = "seller_no")
+    val sellerNo: Long,
+    val sellPrice: BigDecimal? = BigDecimal.ZERO,
+    val supplyPrice: BigDecimal? = BigDecimal.ZERO,
+    val promotionAmount: BigDecimal? = BigDecimal.ZERO,
+    val defaultDeliveryAmount: BigDecimal? = BigDecimal.valueOf(3000),
+    val mileageUsageAmount: BigDecimal? = BigDecimal.ZERO,
+    val itemCategory: Int? = 0,
+    val taxRate: Int? = 3,
+    @Convert(converter = TaxTypeConverter::class)
+    val taxType: TaxType? = TaxType.TAX,
+    @ManyToOne
+    @JoinColumn(name = "seller_no", insertable = false, updatable = false)
+    val seller: Seller,
+    @ManyToOne
+    @JoinColumn(name = "product_no", insertable = false, updatable = false)
+    val product: Product,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as OrderItemSnapshot
+
+        if (id != other.id) return false
+        if (createdAt != other.createdAt) return false
+        if (updatedAt != other.updatedAt) return false
+        if (deletedAt != other.deletedAt) return false
+        if (productNo != other.productNo) return false
+        if (sellerNo != other.sellerNo) return false
+        if (sellPrice != other.sellPrice) return false
+        if (supplyPrice != other.supplyPrice) return false
+        if (promotionAmount != other.promotionAmount) return false
+        if (defaultDeliveryAmount != other.defaultDeliveryAmount) return false
+        if (mileageUsageAmount != other.mileageUsageAmount) return false
+        if (itemCategory != other.itemCategory) return false
+        if (taxRate != other.taxRate) return false
+        if (taxType != other.taxType) return false
+        if (seller != other.seller) return false
+        if (product != other.product) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + (createdAt?.hashCode() ?: 0)
+        result = 31 * result + (updatedAt?.hashCode() ?: 0)
+        result = 31 * result + (deletedAt?.hashCode() ?: 0)
+        result = 31 * result + productNo.hashCode()
+        result = 31 * result + sellerNo.hashCode()
+        result = 31 * result + (sellPrice?.hashCode() ?: 0)
+        result = 31 * result + (supplyPrice?.hashCode() ?: 0)
+        result = 31 * result + (promotionAmount?.hashCode() ?: 0)
+        result = 31 * result + (defaultDeliveryAmount?.hashCode() ?: 0)
+        result = 31 * result + (mileageUsageAmount?.hashCode() ?: 0)
+        result = 31 * result + (itemCategory ?: 0)
+        result = 31 * result + (taxRate ?: 0)
+        result = 31 * result + (taxType?.hashCode() ?: 0)
+        result = 31 * result + seller.hashCode()
+        result = 31 * result + product.hashCode()
+        return result
+    }
+}
