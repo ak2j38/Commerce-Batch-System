3768da9b9b5c10fd20b7e86f91194e718500cf5c
WooJin Park(Ader) <ak2j38@gmail.com>
2024-01-06 00:05:02 +0900

    feat: 정산 테이블 구현

---
+++ src/main/kotlin/com/ader/commercebatchsystem/domain/entity/settlement/SettlementDaily.kt
@@ -0,0 +1,105 @@
+package com.ader.commercebatchsystem.domain.entity.settlement
+
+import com.ader.commercebatchsystem.domain.enums.TaxType
+import jakarta.persistence.*
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.ZonedDateTime
+
+@Entity
+class SettlementDaily(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="settlement_id", nullable = false)
+    val id: Long? = null,
+    @Column(nullable = false)
+    val settlementDate: LocalDate,
+    @Column(nullable = false)
+    val orderNo: Long,
+    @Column(nullable = false)
+    val orderItemNo: Long,
+    @Column(nullable = false)
+    val orderCount: Int? = 1,
+    @Column(nullable = false)
+    val claimReceiptNo: Long? = null,
+    @Column(nullable = false)
+    val sellerNo: Long,
+    @Column(nullable = false)
+    val sellerName: String,
+    val sellerBusinessNumber: Int?,
+    val taxType: TaxType? = TaxType.TAX,
+    val sellType: String? = "CONSIGNMENT",
+    @Column(nullable = false)
+    val pgSalesAmount: BigDecimal? = BigDecimal.ZERO,
+    @Column(nullable = false)
+    val couponDiscountAmount: BigDecimal? = BigDecimal.ZERO,
+    @Column(nullable = false)
+    val mileageUsageAmount: BigDecimal? = BigDecimal.ZERO,
+    @Column(nullable = false)
+    val shippingFeeAmount: BigDecimal? = BigDecimal.ZERO,
+    @Column(nullable = false)
+    val claimShippingFeeAmount: BigDecimal? = BigDecimal.ZERO,
+    @Column(nullable = false)
+    val commissionAmount: BigDecimal? = BigDecimal.ZERO,
+    @Column(nullable = false)
+    val taxAmount: BigDecimal? = BigDecimal.ZERO,
+    val createdAt: ZonedDateTime? = ZonedDateTime.now(), //생성시간
+    val updatedAt: ZonedDateTime? = ZonedDateTime.now(), //업데이트시간
+    val deletedAt: ZonedDateTime? = null, //삭제시간
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SettlementDaily
+
+        if (id != other.id) return false
+        if (settlementDate != other.settlementDate) return false
+        if (orderNo != other.orderNo) return false
+        if (orderItemNo != other.orderItemNo) return false
+        if (orderCount != other.orderCount) return false
+        if (claimReceiptNo != other.claimReceiptNo) return false
+        if (sellerNo != other.sellerNo) return false
+        if (sellerName != other.sellerName) return false
+        if (sellerBusinessNumber != other.sellerBusinessNumber) return false
+        if (taxType != other.taxType) return false
+        if (sellType != other.sellType) return false
+        if (pgSalesAmount != other.pgSalesAmount) return false
+        if (couponDiscountAmount != other.couponDiscountAmount) return false
+        if (mileageUsageAmount != other.mileageUsageAmount) return false
+        if (shippingFeeAmount != other.shippingFeeAmount) return false
+        if (claimShippingFeeAmount != other.claimShippingFeeAmount) return false
+        if (commissionAmount != other.commissionAmount) return false
+        if (taxAmount != other.taxAmount) return false
+        if (createdAt != other.createdAt) return false
+        if (updatedAt != other.updatedAt) return false
+        if (deletedAt != other.deletedAt) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + settlementDate.hashCode()
+        result = 31 * result + orderNo.hashCode()
+        result = 31 * result + orderItemNo.hashCode()
+        result = 31 * result + (orderCount ?: 0)
+        result = 31 * result + (claimReceiptNo?.hashCode() ?: 0)
+        result = 31 * result + sellerNo.hashCode()
+        result = 31 * result + sellerName.hashCode()
+        result = 31 * result + (sellerBusinessNumber ?: 0)
+        result = 31 * result + (taxType?.hashCode() ?: 0)
+        result = 31 * result + (sellType?.hashCode() ?: 0)
+        result = 31 * result + (pgSalesAmount?.hashCode() ?: 0)
+        result = 31 * result + (couponDiscountAmount?.hashCode() ?: 0)
+        result = 31 * result + (mileageUsageAmount?.hashCode() ?: 0)
+        result = 31 * result + (shippingFeeAmount?.hashCode() ?: 0)
+        result = 31 * result + (claimShippingFeeAmount?.hashCode() ?: 0)
+        result = 31 * result + (commissionAmount?.hashCode() ?: 0)
+        result = 31 * result + (taxAmount?.hashCode() ?: 0)
+        result = 31 * result + (createdAt?.hashCode() ?: 0)
+        result = 31 * result + (updatedAt?.hashCode() ?: 0)
+        result = 31 * result + (deletedAt?.hashCode() ?: 0)
+        return result
+    }
+}
