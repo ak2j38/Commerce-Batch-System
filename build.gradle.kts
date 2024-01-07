f271f8f282c3dca415d0228af44f6a54e7e037b0
WooJin Park(Ader) <ak2j38@gmail.com>
2024-01-07 17:46:35 +0900

    build: 쿼리DSL 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
   kotlin("jvm") version "1.8.22"
   kotlin("plugin.spring") version "1.8.22"
   kotlin("plugin.jpa") version "1.8.22"
+  kotlin("kapt") version "1.8.22"
 }
 
 group = "com.ader"
@@ -20,6 +21,9 @@ repositories {
 }
 
 dependencies {
+  implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+  kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+
   implementation("org.springframework.boot:spring-boot-starter-batch")
   implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
