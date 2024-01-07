efa9266b31c202fe1fcd2cb30983367aa311b170
WooJin Park(Ader) <ak2j38@gmail.com>
2024-01-08 00:01:28 +0900

    feat: Reader에서 사용하던 쿼리 확장성을 위해 querydsl로 전환

--- src/main/kotlin/com/ader/commercebatchsystem/core/job/purchaseconfirmed/DeliveryCompletedItemReaderConfig.kt
+++ src/main/kotlin/com/ader/commercebatchsystem/core/job/purchaseconfirmed/DeliveryCompletedItemReaderConfig.kt
@@ -2,24 +2,27 @@ package com.ader.commercebatchsystem.core.job.purchaseconfirmed
 
 import com.ader.commercebatchsystem.domain.entity.order.OrderItem
 import com.ader.commercebatchsystem.infrastructure.data.repository.OrderItemRepository
-import org.springframework.batch.item.data.RepositoryItemReader
-import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder
+import org.springframework.batch.item.database.JpaPagingItemReader
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.domain.Sort
+import java.time.ZonedDateTime
 
 @Configuration
 class DeliveryCompletedItemReaderConfig {
     val chunkSize = 500
 
     @Bean
-    fun deliveryCompletedJpaItemReader(orderItemRepository: OrderItemRepository): RepositoryItemReader<OrderItem> {
-        return RepositoryItemReaderBuilder<OrderItem>()
+    fun deliveryCompletedJpaItemReader(orderItemRepository: OrderItemRepository): JpaPagingItemReader<OrderItem> {
+        val queryProvider = DeliveryCompletedJpaQueryProvider(
+            startDateTime = ZonedDateTime.now().minusDays(1),
+            endDateTime = ZonedDateTime.now(),
+        )
+
+        return JpaPagingItemReaderBuilder<OrderItem>()
             .name("deliveryCompletedItemReader")
-            .repository(orderItemRepository)
-            .methodName("findAll")
             .pageSize(chunkSize)
-            .sorts(mapOf("shippedCompleteAt" to Sort.Direction.ASC))
+            .queryProvider(queryProvider)
             .build()
     }
 }
