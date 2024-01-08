a0cc3164bdf45f0c4f8aae85c3c4e66c577e9dcc
WooJin Park(Ader) <ak2j38@gmail.com>
2024-01-08 22:15:30 +0900

    build: web 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
   implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
   kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
 
+  implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-batch")
   implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
